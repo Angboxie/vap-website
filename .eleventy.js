42680280e590a54662cfa733faf8859c20c6f1e0
Angbo Xie <angbox@student.unimelb.edu.au>
2026-08-26 10:21:14 +1000

    Eleventy setup, base layouts, base.css

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/styles");
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_includes/layouts",
+      data: "_data",
+      output: "_site",
+    },
+    templateFormats: ["njk", "md"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+  };
+};
